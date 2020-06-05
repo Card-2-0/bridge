2956b189a53a2e31c3c86c94317fe7a8476ed6f2
kaousheik <kaousheik@gmail.com>
2020-06-05 17:10:05 +0530

    max player added

--- web/src/components/Messages.tsx
+++ web/src/components/Messages.tsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 import { Trump } from "./Trump";
-import { UserCards } from "./UserCards"
+import { UserCards } from "./UserCards";
 import { Game } from "./Game";
 import { TargetChoose } from "./TargetChoose";
 
 const ENDPOINT = "http://localhost:8080/";
 let socket: any;
-let tmp:any = null;
-const arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+let tmp: any = null;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export const Messages = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const [usersinfo, setUsersInfo] = useState(tmp)
+  const [usersinfo, setUsersInfo] = useState(tmp);
   const [id, setId] = useState(-1);
   const [cards, setCards] = useState([{ suit: "", value: "" }]);
   const [trumpChoose, setTrumpChoose] = useState(false);
   const [trump, setTrump] = useState("");
-  const [num, setNum] = useState(0)
-  const [game, setGame] = useState(false)
-  const [target, setTarget] = useState([-1,-1])
-  const [targetChoose, setTargetChoose] = useState(-1)
-
+  const [num, setNum] = useState(0);
+  const [game, setGame] = useState(false);
+  const [target, setTarget] = useState([-1, -1]);
+  const [targetChoose, setTargetChoose] = useState(-1);
+  const [trumpPlayer, setTrumpPlayer] = useState(-1);
   useEffect(() => {
     socket = io(ENDPOINT);
     const { name, room }: any = queryString.parse(window.location.search);
@@ -37,58 +37,73 @@ export const Messages = () => {
   }, [ENDPOINT]);
 
   useEffect(() => {
-    socket.on("cards", (dcards: any, id: number, roomusers:any) => {
+    socket.on("cards", (dcards: any, id: number, roomusers: any) => {
       setCards(dcards);
       setId(id);
       setUsersInfo(roomusers);
     });
-    socket.on("trumpTurn", (playerid:number, trumpsuit:string, trumpvalue:string) => {
-      if(playerid === id) setTrumpChoose(true)
-      else setTrumpChoose(false)
-      setTrump(trumpsuit)
-      setNum(parseInt(trumpvalue)+1)
-    })
-    socket.on("trumpDone", (finalTrump:string, targets:number[]) =>{
-      setTrump(finalTrump)
-      setTrumpChoose(false)
-      setTarget(targets)
-    })
-    socket.on("targetChoose", (t:number) => {
-      if(t === id%2) { setTargetChoose(0) }
-      else setTargetChoose(2)
-    })
-    socket.on('targetSelectDone', (targets:number[]) => {
-      setTarget(targets)
-      setGame(true)
-      setTargetChoose(-1)
-    })
+    socket.on(
+      "trumpTurn",
+      (playerid: number, trumpsuit: string, trumpvalue: string, pid: any) => {
+        if (playerid === id) setTrumpChoose(true);
+        else setTrumpChoose(false);
+        setTrump(trumpsuit);
+        setNum(parseInt(trumpvalue) + 1);
+        if (pid) setTrumpPlayer(parseInt(pid) + 1);
+      }
+    );
+    socket.on("trumpDone", (finalTrump: string, targets: number[]) => {
+      setTrump(finalTrump);
+      setTrumpChoose(false);
+      setTarget(targets);
+    });
+    socket.on("targetChoose", (t: number) => {
+      if (t === id % 2) {
+        setTargetChoose(0);
+      } else setTargetChoose(2);
+    });
+    socket.on("targetSelectDone", (targets: number[]) => {
+      setTarget(targets);
+      setGame(true);
+      setTargetChoose(-1);
+    });
   });
 
-  const onTrump = (trumpSuit:string , trumpValue:string , pass:boolean) => {
-    console.log(trumpValue, trumpSuit, pass)
-    socket.emit('trumpPlay',trumpValue, trumpSuit, pass,room,id)
-  }
+  const onTrump = (trumpSuit: string, trumpValue: string, pass: boolean) => {
+    console.log(trumpValue, trumpSuit, pass);
+    socket.emit("trumpPlay", trumpValue, trumpSuit, pass, room, id);
+  };
 
-  const onChoose = (tar:number) => {
-    console.log(tar)
-    setTargetChoose(1)
-    socket.emit('targetSelect', id, tar, room)
-  }
+  const onChoose = (tar: number) => {
+    console.log(tar);
+    setTargetChoose(1);
+    socket.emit("targetSelect", id, tar, room);
+  };
   return (
     <div>
       <h1>Hi {name ? name : "There"}</h1>
       {id != -1 && <p>You are Player no. {id + 1}</p>}
-      {usersinfo && 
+      {usersinfo && (
         <div>
           <h4>Teams in Room</h4>
-          <p>Team 1 : {usersinfo[0].name} (Player 1) and {usersinfo[2].name} (Player 3)</p>
-          <p>Team 2 : {usersinfo[1].name} (Player 2) and {usersinfo[3].name} (Player 4)</p>
-        </div>}
-      {cards.length > 1 && <UserCards cards = {cards} /> }
+          <p>
+            Team 1 : {usersinfo[0].name} (Player 1) and {usersinfo[2].name}{" "}
+            (Player 3)
+          </p>
+          <p>
+            Team 2 : {usersinfo[1].name} (Player 2) and {usersinfo[3].name}{" "}
+            (Player 4)
+          </p>
+        </div>
+      )}
+      {cards.length > 1 && <UserCards cards={cards} game={game} />}
       {trumpChoose && <Trump num={num} handleSubmit={onTrump} trump={trump} />}
-      { (targetChoose === -1 && !game && trump) && <p>Current Trump : {trump}</p>}
-      {targetChoose !== -1 &&  <TargetChoose handleSubmit={onChoose} op={targetChoose} trump={trump} /> }
-      {game && <Game trump = {trump} target={target}/>}
+      {targetChoose === -1 && !game && trump && <p>Current Trump : {trump}</p>}
+      {trumpPlayer !== -1 && <p>Trump Placed By {trumpPlayer}</p>}
+      {targetChoose !== -1 && (
+        <TargetChoose handleSubmit={onChoose} op={targetChoose} trump={trump} />
+      )}
+      {game && <Game trump={trump} target={target} />}
     </div>
   );
 };
--- web/src/components/UserCards.tsx
+++ web/src/components/UserCards.tsx
@@ -1,24 +1,42 @@
-import React from "react"
+import React, { useState } from "react";
 
-export const UserCards = ({cards}:any) => {
-    return(
-        <div className="playingCards fourColours rotateHand">
-            <h3>Your Cards Are</h3>
-            <ul className="table">
-            {cards.map((card:any) => {
-                const lowerValue = card.value.toLowerCase();
-                const suitLower = card.suit.toLowerCase();
-                // console.log(suitLower);
-                return (
-                <li>
-                    <a className={`card rank-${lowerValue} ${suitLower}`}>
-                    <span className="rank">{card.value}</span>
-                    <span className="suit">&diams;</span>
-                    </a>
-                </li>
-                );
-            })}
-            </ul>
-        </div>
-    )
-}
+export const UserCards = ({ cards, game }: any) => {
+  const [suit, setSuit] = useState<string>("");
+  const [val, setVal] = useState<string>("");
+  const handleDispatch = () => {};
+  return (
+    <div className="playingCards fourColours rotateHand">
+      <h3>Your Cards Are</h3>
+      <ul className="table">
+        {cards.map((card: any, i: any) => {
+          const lowerValue = card.value.toLowerCase();
+          const suitLower = card.suit.toLowerCase();
+          // console.log(suitLower);
+          return (
+            <li
+              key={i}
+              value={`${card.suit} ${card.value}`}
+              onClick={(e) => {
+                console.log(e.currentTarget.value);
+              }}
+            >
+              <a className={`card rank-${lowerValue} ${suitLower}`}>
+                <span className="rank">{card.value}</span>
+                <span className="suit">&diams;</span>
+                {game && (
+                  <input
+                    type="checkbox"
+                    name="c-10C"
+                    id="c-10C"
+                    value="select"
+                  />
+                )}
+              </a>
+            </li>
+          );
+        })}
+        <button onClick={handleDispatch}>Dispatch Card</button>
+      </ul>
+    </div>
+  );
+};
