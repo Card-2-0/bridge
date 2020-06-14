7fcb5e3ae353d485a3959a6f15d4bb42e2c98c57
kaousheik <kaousheik@gmail.com>
2020-06-14 18:26:15 +0530

    cards on table bug resolved

--- web/src/components/CardsOnTable.tsx
+++ web/src/components/CardsOnTable.tsx
@@ -6,12 +6,18 @@ const spades = <span className="suit">&spades;</span>;
 
 const convert = ["J", "Q", "K", "A"];
 export const CardsOnTable = ({ cards, users }: any) => {
-  const [u1, setU1] = useState<any>();
-  const [u2, setU2] = useState<any>();
-  const [u3, setU3] = useState<any>();
-  const [u4, setU4] = useState<any>();
+  const [u1, setU1] = useState<any>(undefined);
+  const [u2, setU2] = useState<any>(undefined);
+  const [u3, setU3] = useState<any>(undefined);
+  const [u4, setU4] = useState<any>(undefined);
   useEffect(() => {
     if (cards) {
+      if (cards.length === 1) {
+        setU1(undefined);
+        setU2(undefined);
+        setU3(undefined);
+        setU4(undefined);
+      }
       if (cards[cards.length - 1].id === 0) setU1(cards[cards.length - 1]);
       else if (cards[cards.length - 1].id === 1) setU2(cards[cards.length - 1]);
       else if (cards[cards.length - 1].id === 2) setU3(cards[cards.length - 1]);
@@ -21,102 +27,122 @@ export const CardsOnTable = ({ cards, users }: any) => {
   return (
     <div className="table-cards">
       <h3>Cards on Table</h3>
-      
+
       <div className="component playingCards fourColours rotateHand">
         <div className="grid-container">
           <div className="item1 t1">
             <h3>{users && users[0].name}</h3>
             <div className="card-on-table">
-            {u1 ? (
-              <a
-                className={`card rank-${
-                  u1.value > 10
-                    ? convert[parseInt(u1.value) - 11].toLowerCase()
-                    : u1.value
-                } ${u1.suit.toLowerCase()}`}
-              >
-                <span className="rank">
-                  {u1.value > 10 ? convert[parseInt(u1.value) - 11] : u1.value}
-                </span>
-                {u1.suit === "HEARTS" && hearts}
-                {u1.suit === "CLUBS" && clubs}
-                {u1.suit === "SPADES" && spades}
-                {u1.suit === "DIAMS" && diams}
-              </a>
-            ) : (
-              <p><i className="fas fa-5x fa-question"></i></p>
-            )}</div>
+              {u1 ? (
+                <a
+                  className={`card rank-${
+                    u1.value > 10
+                      ? convert[parseInt(u1.value) - 11].toLowerCase()
+                      : u1.value
+                  } ${u1.suit.toLowerCase()}`}
+                >
+                  <span className="rank">
+                    {u1.value > 10
+                      ? convert[parseInt(u1.value) - 11]
+                      : u1.value}
+                  </span>
+                  {u1.suit === "HEARTS" && hearts}
+                  {u1.suit === "CLUBS" && clubs}
+                  {u1.suit === "SPADES" && spades}
+                  {u1.suit === "DIAMS" && diams}
+                </a>
+              ) : (
+                <p>
+                  <i className="fas fa-5x fa-question"></i>
+                </p>
+              )}
+            </div>
           </div>
           <div className="item1 t2">
             <h3>{users && users[1].name}</h3>
             <div className="card-on-table">
-            {u2 ? (
-              <a
-                className={`card rank-${
-                  u2.value > 10
-                    ? convert[parseInt(u2.value) - 11].toLowerCase()
-                    : u2.value
-                } ${u2.suit.toLowerCase()}`}
-              >
-                <span className="rank">
-                  {u2.value > 10 ? convert[parseInt(u2.value) - 11] : u2.value}
-                </span>
-                {u2.suit === "HEARTS" && hearts}
-                {u2.suit === "CLUBS" && clubs}
-                {u2.suit === "SPADES" && spades}
-                {u2.suit === "DIAMS" && diams}
-              </a>
-            ) : (
-              <p><i className="fas fa-5x fa-question"></i></p>
-            )}</div>
+              {u2 ? (
+                <a
+                  className={`card rank-${
+                    u2.value > 10
+                      ? convert[parseInt(u2.value) - 11].toLowerCase()
+                      : u2.value
+                  } ${u2.suit.toLowerCase()}`}
+                >
+                  <span className="rank">
+                    {u2.value > 10
+                      ? convert[parseInt(u2.value) - 11]
+                      : u2.value}
+                  </span>
+                  {u2.suit === "HEARTS" && hearts}
+                  {u2.suit === "CLUBS" && clubs}
+                  {u2.suit === "SPADES" && spades}
+                  {u2.suit === "DIAMS" && diams}
+                </a>
+              ) : (
+                <p>
+                  <i className="fas fa-5x fa-question"></i>
+                </p>
+              )}
+            </div>
           </div>
         </div>
         <div className="grid-container">
           <div className="item1 t2">
             <h3>{users && users[3].name}</h3>
             <div className="card-on-table">
-            {u4 ? (
-              <a
-                className={`card rank-${
-                  u4.value > 10
-                    ? convert[parseInt(u4.value) - 11].toLowerCase()
-                    : u4.value
-                } ${u4.suit.toLowerCase()}`}
-              > 
-                <span className="rank">
-                  {u4.value > 10 ? convert[parseInt(u4.value) - 11] : u4.value}
-                </span>
-                {u4.suit === "HEARTS" && hearts}
-                {u4.suit === "CLUBS" && clubs}
-                {u4.suit === "SPADES" && spades}
-                {u4.suit === "DIAMS" && diams}
-              </a>
-            ) : (
-              <p><i className="fas fa-5x fa-question"></i></p>
-            )}</div>
+              {u4 ? (
+                <a
+                  className={`card rank-${
+                    u4.value > 10
+                      ? convert[parseInt(u4.value) - 11].toLowerCase()
+                      : u4.value
+                  } ${u4.suit.toLowerCase()}`}
+                >
+                  <span className="rank">
+                    {u4.value > 10
+                      ? convert[parseInt(u4.value) - 11]
+                      : u4.value}
+                  </span>
+                  {u4.suit === "HEARTS" && hearts}
+                  {u4.suit === "CLUBS" && clubs}
+                  {u4.suit === "SPADES" && spades}
+                  {u4.suit === "DIAMS" && diams}
+                </a>
+              ) : (
+                <p>
+                  <i className="fas fa-5x fa-question"></i>
+                </p>
+              )}
+            </div>
           </div>
           <div className="item1 t1">
             <h3>{users && users[2].name}</h3>
             <div className="card-on-table">
-            {u3 ? (
-              <a
-                className={`card rank-${
-                  u3.value > 10
-                    ? convert[parseInt(u3.value) - 11].toLowerCase()
-                    : u3.value
-                } ${u3.suit.toLowerCase()}`}
-              >
-                <span className="rank">
-                  {u3.value > 10 ? convert[parseInt(u3.value) - 11] : u3.value}
-                </span>
-                {u3.suit === "HEARTS" && hearts}
-                {u3.suit === "CLUBS" && clubs}
-                {u3.suit === "SPADES" && spades}
-                {u3.suit === "DIAMS" && diams}
-              </a>
-            ) : (
-              <p><i className="fas fa-5x fa-question"></i></p>
-            )}</div>
+              {u3 ? (
+                <a
+                  className={`card rank-${
+                    u3.value > 10
+                      ? convert[parseInt(u3.value) - 11].toLowerCase()
+                      : u3.value
+                  } ${u3.suit.toLowerCase()}`}
+                >
+                  <span className="rank">
+                    {u3.value > 10
+                      ? convert[parseInt(u3.value) - 11]
+                      : u3.value}
+                  </span>
+                  {u3.suit === "HEARTS" && hearts}
+                  {u3.suit === "CLUBS" && clubs}
+                  {u3.suit === "SPADES" && spades}
+                  {u3.suit === "DIAMS" && diams}
+                </a>
+              ) : (
+                <p>
+                  <i className="fas fa-5x fa-question"></i>
+                </p>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- web/src/components/UserCards.tsx
+++ web/src/components/UserCards.tsx
@@ -17,84 +17,88 @@ export const UserCards = ({ cards, game, handleDispatch, roundSuit }: any) => {
   return (
     <div className="user-cards playingCards fourColours rotateHand">
       {game ? (
-        <h3>Its Your Turn. Pick a card or a Highlighted Card</h3>
+        <h3>Its Your Turn. Pick a Highlighted Card</h3>
       ) : (
         <h3>Please wait for your turn...</h3>
       )}
       <h3>Your Cards Are</h3>
       <div className="user-table">
-      <ul className="table">
-        {cards.map((card: any, i: any) => {
-          const lowerValue = card.value.toLowerCase();
-          const suitLower = card.suit.toLowerCase();
-          return card.value === val && card.suit === suit ? (
-            <li
-              key={i}
-              value={`${card.suit} ${card.value}`}
-              onClick={() => {
-                if (game && (anyCard || card.suit === roundSuit)) {
-                  setVal(card.value);
-                  setSuit(card.suit);
-                }
-              }}
-            >
-              <a
-                className={
-                  card.suit === roundSuit
-                    ? `card rank-${lowerValue} ${suitLower} available`
-                    : `card rank-${lowerValue} ${suitLower}`
-                }
-                style={{ marginTop: "-1rem" }}
+        <ul className="table">
+          {cards.map((card: any, i: any) => {
+            const lowerValue = card.value.toLowerCase();
+            const suitLower = card.suit.toLowerCase();
+            return card.value === val && card.suit === suit ? (
+              <li
+                key={i}
+                value={`${card.suit} ${card.value}`}
+                onClick={() => {
+                  if (game && (anyCard || card.suit === roundSuit)) {
+                    setVal(card.value);
+                    setSuit(card.suit);
+                  }
+                }}
               >
-                <span className="rank">{card.value}</span>
-                {suitLower === "hearts" && hearts}
-                {suitLower === "clubs" && clubs}
-                {suitLower === "spades" && spades}
-                {suitLower === "diams" && diams}
-              </a>
-            </li>
-          ) : (
-            <li
-              key={i}
-              value={`${card.suit} ${card.value}`}
-              onClick={() => {
-                if (game && (anyCard || card.suit === roundSuit)) {
-                  setVal(card.value);
-                  setSuit(card.suit);
-                }
-              }}
-            >
-              <a
-                className={
-                  card.suit === roundSuit
-                    ? `card rank-${lowerValue} ${suitLower} available`
-                    : `card rank-${lowerValue} ${suitLower}`
-                }
+                <a
+                  className={
+                    !anyCard
+                      ? roundSuit === card.suit
+                        ? `card rank-${lowerValue} ${suitLower} available`
+                        : `card rank-${lowerValue} ${suitLower}`
+                      : `card rank-${lowerValue} ${suitLower} available`
+                  }
+                  style={{ marginTop: "-1rem" }}
+                >
+                  <span className="rank">{card.value}</span>
+                  {suitLower === "hearts" && hearts}
+                  {suitLower === "clubs" && clubs}
+                  {suitLower === "spades" && spades}
+                  {suitLower === "diams" && diams}
+                </a>
+              </li>
+            ) : (
+              <li
+                key={i}
+                value={`${card.suit} ${card.value}`}
+                onClick={() => {
+                  if (game && (anyCard || card.suit === roundSuit)) {
+                    setVal(card.value);
+                    setSuit(card.suit);
+                  }
+                }}
               >
-                <span className="rank">{card.value}</span>
-                {suitLower === "hearts" && hearts}
-                {suitLower === "clubs" && clubs}
-                {suitLower === "spades" && spades}
-                {suitLower === "diams" && diams}
-              </a>
-            </li>
-          );
-        })}
+                <a
+                  className={
+                    !anyCard
+                      ? roundSuit === card.suit
+                        ? `card rank-${lowerValue} ${suitLower} available`
+                        : `card rank-${lowerValue} ${suitLower}`
+                      : `card rank-${lowerValue} ${suitLower} available`
+                  }
+                >
+                  <span className="rank">{card.value}</span>
+                  {suitLower === "hearts" && hearts}
+                  {suitLower === "clubs" && clubs}
+                  {suitLower === "spades" && spades}
+                  {suitLower === "diams" && diams}
+                </a>
+              </li>
+            );
+          })}
         </ul>
-        </div>
-        <br/>
-        <div hidden={!game}>
-          <button
-            onClick={() => {
-              handleDispatch(val, suit);
-              setSuit("");
-              setVal("");
-            }}
-            disabled={!val && !suit}
-          >
-            Dispatch Card
-          </button>
-        </div>
+      </div>
+      <br />
+      <div hidden={!game}>
+        <button
+          onClick={() => {
+            handleDispatch(val, suit);
+            setSuit("");
+            setVal("");
+          }}
+          disabled={!val && !suit}
+        >
+          Dispatch Card
+        </button>
+      </div>
     </div>
   );
 };
